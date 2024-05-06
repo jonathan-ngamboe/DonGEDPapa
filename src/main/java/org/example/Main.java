2b532c582f1ceaf049e02f8b4e7f8dbb7cf53a7d
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-06 21:44:38 +0200

    .Addition of a test code to retrieve a token

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.api.ApiClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApiClient client = new ApiClient("http://157.26.83.80:2240/");
+        try {
+            String token = client.getToken("admin3", "admin3");
+            System.out.println("Token: " + token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
