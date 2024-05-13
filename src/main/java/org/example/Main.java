812cde1c9da63e414af11832d32923f2adccfe94
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-13 21:00:55 +0200

    Added the ability to retrieve expense reports using the advanced search function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,21 @@
 package org.example;
 
+import org.codehaus.jettison.json.JSONObject;
 import org.example.api.ApiClient;
 
 public class Main {
     public static void main(String[] args) {
         ApiClient client = new ApiClient("http://157.26.83.80:2240/");
         try {
-            String token = client.getToken("admin3", "admin3");
-            System.out.println("Token: " + token);
+            // Get token
+            String token = client.getToken("admin2", "admin2");
+
+            // Advanceed search request
+            JSONObject query = new JSONObject();
+            query.put("searchPattern", "(;NDF_etat|l01|Remboursement effectué|list;)");
+            query.put("contentTypeIDs", "101");
+            String notes_de_frais = client.advancedSearchRequest(query).toString();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
