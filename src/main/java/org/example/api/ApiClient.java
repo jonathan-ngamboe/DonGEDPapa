efe3a0f03f1869c23d799e0b318959399e4b4584
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-13 21:01:22 +0200

    Added an advanced search method

--- src/main/java/org/example/api/ApiClient.java
+++ src/main/java/org/example/api/ApiClient.java
@@ -11,6 +11,7 @@ import java.net.URLEncoder;
 public class ApiClient {
 
     private String baseUrl;
+    private String token;
 
     public ApiClient(String baseUrl) {
         this.baseUrl = baseUrl;
@@ -18,8 +19,8 @@ public class ApiClient {
 
     public String getToken(String username, String password) throws Exception {
         String url = baseUrl + "/token";
+        String response = null;
         HttpURLConnection connection = null;
-        String token;
 
         String requestBody =  "grant_type=password&username=" + URLEncoder.encode(username, "UTF-8") + "&password=" + URLEncoder.encode(password, "UTF-8");
 
@@ -27,13 +28,36 @@ public class ApiClient {
             connection = JSONUtilities.write(url, JSONUtilities.RequestMethod.POST, requestBody, null);
 
             if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                token = JSONUtilities.read(connection);
-                return new JSONObject(token).getString("access_token");
+                response = JSONUtilities.read(connection);
+                this.token = new JSONObject(response).getString("access_token");
+                return token;
             } else {
                 throw new IOException("Failed : HTTP error code : " + connection.getResponseCode());
             }
         } catch (IOException e) {
             throw new Exception("Error while getting token", e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public String advancedSearchRequest(JSONObject query) throws Exception {
+        String url = baseUrl + "api/search/advanced";
+        HttpURLConnection connection = null;
+
+        try {
+            connection = JSONUtilities.write(url, JSONUtilities.RequestMethod.POST, query.toString(), token);
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String response = JSONUtilities.read(connection);
+                return response;
+            } else {
+                throw new IOException("Failed : HTTP error code : " + connection.getResponseCode());
+            }
+        } catch (IOException | JSONException e) {
+            throw new Exception("Error while making advanced search request", e);
         } finally {
             if (connection != null) {
                 connection.disconnect();
