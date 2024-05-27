70a545b299fb3c99b448b6a557c50255acafab4f
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-05-27 20:40:29 +0200

    Added validation of expense claims

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.example.api.ApiClient;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +14,23 @@ public class Main {
 
             // Advanceed search request
             JSONObject query = new JSONObject();
-            query.put("searchPattern", "(;NDF_etat|l01|Remboursement effectué|list;)");
-            query.put("contentTypeIDs", "101");
+            query.put("searchPattern", "(;NDF_etat|l01|Remboursement effectué - En attente d'exportation|list;)");
+            query.put("contentTypeIDs", "140");
             String notes_de_frais = client.advancedSearchRequest(query).toString();
+
+            // Change all the states to "Remboursement effectué - Exporté"
+            JSONArray jsonArray = new JSONArray(notes_de_frais); // Parse the JSON data to a JSONArray
+            // Iterate through the array
+            for (int i = 0; i < jsonArray.length(); i++) {
+                // Get each JSONObject
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                // Extract the ObjectID
+                int objectId = jsonObject.getInt("ObjectID");
+
+                // Validate
+                client.validate(objectId);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
